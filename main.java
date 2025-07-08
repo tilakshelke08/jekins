8fac68e8f8df6b42dd3e9b3ae9ba228ff9b976b5
(Tilakshelke08) <(Tilakshelke123@gmail.com)>
2025-07-08 12:40:32 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+
+class main {
+
+  public static void main(String[] args) {
+  System.out.println("This is First jenkins program ");
+  System.out.println("Thanks for  reach again "); 
+  System.out.println(new Date());
+  }
+}
